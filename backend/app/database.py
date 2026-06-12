90b2034cb2eb0efc2e54bf1f90ae5f4a637f789c
dummydevelopment <dummydevelopment.020@gmail.com>
2026-06-12 08:39:40 +0530

    this works

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,44 +4,19 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
-
-print("\n========== DATABASE CONFIG ==========")
-
 print("DB_HOST =", settings.DB_HOST)
 print("DB_NAME =", settings.DB_NAME)
 print("DB_USER =", settings.DB_USER)
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-print("\nDATABASE_URL exists =", DATABASE_URL is not None)
-
-if DATABASE_URL:
-    # hide password before printing
-    safe_url = DATABASE_URL
-
-    if "@" in safe_url and "://" in safe_url:
-        protocol = safe_url.split("://")[0]
-        remaining = safe_url.split("://")[1]
-
-        if ":" in remaining:
-            user = remaining.split(":")[0]
-            host_part = remaining.split("@")[-1]
-
-            safe_url = (
-                f"{protocol}://"
-                f"{user}:******@"
-                f"{host_part}"
-            )
-
-    print("DATABASE_URL =", safe_url)
-else:
-    print("❌ DATABASE_URL is None")
-
-print("=====================================\n")
+# DATABASE_URL = (
+#     f"postgresql://{settings.DB_USER}:"
+#     f"{settings.DB_PASSWORD}@"
+#     f"{settings.DB_HOST}:"
+#     f"{settings.DB_PORT}/"
+#     f"{settings.DB_NAME}"
+# )
 
 engine = create_engine(DATABASE_URL)
-
-print("✅ SQLAlchemy engine created")
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -49,8 +24,4 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-print("✅ SessionLocal created")
-
 Base = declarative_base()
-
-print("✅ Base created")
