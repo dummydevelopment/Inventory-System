600719fb6f147e5c78f7c3f14331b23d254e2a81
dummydevelopment <dummydevelopment.020@gmail.com>
2026-06-12 08:14:29 +0530

    db connection update

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
 
 from app.config import settings
+
 print("DB_HOST =", settings.DB_HOST)
 print("DB_NAME =", settings.DB_NAME)
 print("DB_USER =", settings.DB_USER)
+
 DATABASE_URL = (
-    f"postgresql://{settings.DB_USER}:"
-    f"{settings.DB_PASSWORD}@"
+    f"postgresql://"
+    f"{settings.DB_USER}:"
+    f"{quote_plus(settings.DB_PASSWORD)}@"
     f"{settings.DB_HOST}:"
     f"{settings.DB_PORT}/"
     f"{settings.DB_NAME}"
